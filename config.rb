d3d5e319a4179051c02caa268d9b49fb08c1605d
Johannes Klumpe <klumpe@live.de>
2017-11-05 14:04:05 +0100

    weekend result

--- config.rb
+++ config.rb
@@ -38,9 +38,18 @@ page '/*.txt', layout: false
 # end
 
 # Build-specific configuration
+
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+
+
+
+configure :build do
+activate :relative_assets
+activate :asset_hash
+end
+
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+end
